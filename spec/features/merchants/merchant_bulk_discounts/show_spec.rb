629f9adc564d607c4d532f1ff068dbcf94163ed9
Charles Ren <cren04@syr.edu>
2023-09-27 04:13:51 -0700

    US 4 Complete

---
+++ spec/features/merchants/merchant_bulk_discounts/show_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe "Merchant Bulk Discounts Show Page", type: :feature do
+  before :each do
+    load_test_data
+
+    @discount1 = BulkDiscount.create!(merchant_id: @merchant_5.id, discount: 10, quantity: 2)
+    @discount2 = BulkDiscount.create!(merchant_id: @merchant_5.id, discount: 15, quantity: 4)
+    @discount3 = BulkDiscount.create!(merchant_id: @merchant_6.id, discount: 33, quantity: 6)
+    @discount4 = BulkDiscount.create!(merchant_id: @merchant_6.id, discount: 50, quantity: 10)
+  end
+
+  # Final US 4
+  it "displays the discount's quantity threshold and percentage discount" do
+    visit merchant_bulk_discount_path(@merchant_5, @discount2)
+
+    expect(page).to have_content("#{@discount2.discount}% off for more than #{@discount2.quantity} items")
+  end
+end
